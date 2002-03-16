d5d247f79e2e5342153cb0b39297d959522f36dd
c95men <c95men@fa5118ba-f80e-0410-a4dc-9a36c028f66c>
2002-03-16 18:23:16 +0000

    Added UCS2 support

--- smsj/src/org/marre/sms/SmsTextMessage.java
+++ smsj/src/org/marre/sms/SmsTextMessage.java
@@ -23,25 +23,51 @@ import org.marre.sms.util.SmsPduUtil;
 
 public class SmsTextMessage extends SmsMessage
 {
-    public static final int TEXT_ENCODING_DEFAULT = 0;
-    public static final int TEXT_ENCODING_DCS2 = 0;
+    public static final int TEXT_ALPHABET_GSM = 0;
+    public static final int TEXT_ALPHABET_UCS2 = 1;
 
-    public SmsTextMessage(String theMsg)
+    public SmsTextMessage(String theMsg, int theAlphabet)
     {
         try
         {
             ByteArrayOutputStream baos = new ByteArrayOutputStream(140);
-            SmsPduUtil.writeSeptets(baos, theMsg);
-            baos.close();
-            setUserData(myUd = baos.toByteArray(), theMsg.length());
+            int userDataLength = 0;
 
-            // 7-bit encoding, Class 1 message
-            setDataCodingScheme((byte)0x00);
+            switch (theAlphabet)
+            {
+            case TEXT_ALPHABET_GSM:
+                SmsPduUtil.writeSeptets(baos, theMsg);
+                userDataLength = theMsg.length();
+                // 7-bit encoding, No message class, No compression
+                setDataCodingScheme((byte)0x00);
+                break;
+            case TEXT_ALPHABET_UCS2:
+                baos.write(theMsg.getBytes("UTF-16BE"));
+                userDataLength = theMsg.length() * 2;
+                // 16 bit UCS2 encoding, No message class, No compression
+                setDataCodingScheme((byte)0x08);
+                break;
+            }
+
+            baos.close();
+            setUserData(myUd = baos.toByteArray(), userDataLength);
+        }
+        catch (UnsupportedEncodingException ex)
+        {
+            // Shouldn't happen. According to the javadoc documentation
+            // for JDK 1.3.1 the "UTF-16BE" is standard...
+            throw new RuntimeException(ex.getMessage());
         }
         catch (IOException ex)
         {
-            // Shouldnt really happen...
-            throw new RuntimeException("IOException in SmsTextMessage");
+            // Shouldnt really happen. We were writing to an internal
+            // stream.
+            throw new RuntimeException(ex.getMessage());
         }
+    }
+
+    public SmsTextMessage(String theMsg)
+    {
+        this(theMsg, TEXT_ALPHABET_GSM);
     }
 }
