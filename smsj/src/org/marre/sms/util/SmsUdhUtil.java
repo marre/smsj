704e61bd8039f5af858648e519663b11e7da703f
c95men <c95men@fa5118ba-f80e-0410-a4dc-9a36c028f66c>
2002-04-16 20:13:28 +0000

    Added some more UDH elements.

--- smsj/src/org/marre/sms/util/SmsUdhUtil.java
+++ smsj/src/org/marre/sms/util/SmsUdhUtil.java
@@ -21,12 +21,30 @@ package org.marre.sms.util;
 import org.marre.sms.SmsUdhElement;
 import org.marre.sms.SmsConstants;
 
+/**
+ * Toolkit class for SmsUdhElement objects
+ * <p>
+ * This class doesn't contain helper methods for all types of UDHI.
+ *
+ * @author Markus Eriksson
+ */
 public class SmsUdhUtil
 {
     private SmsUdhUtil()
     {
     }
 
+    /**
+     * Creates a "8Bit concatenated" UDH element using UDH_IEI_CONCATENATED_8BIT
+     * <p>
+     * This can be used to create a concatenated SMS.
+     *
+     * @param theRefNr The reference number of this SMS, must be the same in
+     * all SMS
+     * @param theTotSms Total number of SMS
+     * @param theSeqNr Sequence number
+     * @return A SmsUdhElement
+     */
     public static final SmsUdhElement getConcatUdh(int theRefNr, int theTotSms, int theSeqNr)
     {
         byte[] udh = new byte[3];
@@ -36,5 +54,107 @@ public class SmsUdhUtil
         udh[2] = (byte) (theSeqNr  & 0xff);
 
         return new SmsUdhElement(SmsConstants.UDH_IEI_CONCATENATED_8BIT, udh);
+    }
+
+    /**
+     * Creates a "Message waiting" UDH element using UDH_IEI_SPECIAL_MESSAGE
+     * <p>
+     * If more than one type of message is required to be indicated within
+     * one SMS message, then multiple "Message waiting" UDH elements must
+     * be used.
+     * <p>
+     * <b>Special handling in concatenated messages:</b><br>
+     * <i>
+     * "In the case where this IEI is to be used in a concatenated SM then the
+     * IEI, its associated IEI length and IEI data shall be contained in the
+     * first segment of the concatenated SM. The IEI, its associated IEI length
+     * and IEI data should also be contained in every subsequent segment of the
+     * concatenated SM although this is not mandatory. However, in the case
+     * where these elements are not contained in every subsequent segment of
+     * the concatenated SM and where an out of sequence segment delivery
+     * occurs or where the first segment is not delivered then processing
+     * difficulties may arise at the receiving entity which may result in
+     * the concatenated SM being totally or partially discarded."
+     * </i>
+     *
+     * @param storeMsg Set to true if the message should be stored
+     * @param theMsgType Message type, may be one of MESSAGE_WAITING_VOICE,
+     * MESSAGE_WAITING_FAX, MESSAGE_WAITING_EMAIL or MESSAGE_WAITING_OTHER.
+     * @param theMsgCount Number of messages waiting for retrieval. Max 255
+     * messages. The value 255 shall be taken to mean 255 or greater.
+     * @return A SmsUdhElement
+     */
+    public static final SmsUdhElement getMessageWaitingUdh(boolean storeMsg, int theMsgType, int theMsgCount)
+    {
+        byte[] udh = new byte[2];
+
+        udh[0] = (byte) (theMsgType  & 0x7f);
+        if ( storeMsg )
+        {
+            udh[0] |= (byte) (0x80);
+        }
+        udh[1] = (byte) (theMsgCount & 0xff);
+
+        return new SmsUdhElement(SmsConstants.UDH_IEI_SPECIAL_MESSAGE, udh);
+    }
+
+    /**
+     * Creates a "8 bit Application Port Adressing" UDH element
+     * using UDH_IEI_APP_PORT_8BIT
+     * <p>
+     * Note! Only values between 240 and 255 are usable, the rest of the port
+     * numbers are marked as reserved.
+     * <p>
+     * <b>Special handling in concatenated messages:</b><br>
+     * <i>
+     * In the case where this IE is to be used in a concatenated SM then the
+     * IEI, its associated IEI length and IEI data shall be contained in the
+     * first segment of the concatenated SM. The IEI, its associated IEI length
+     * and IEI data shall also be contained in every subsequent segment of the
+     * concatenated SM.
+     * </i>
+     * @param theDestPort Destination port
+     * @param theOrigPort Source port
+     * @return A SmsUdhElement
+     */
+    public static final SmsUdhElement get8BitApplicationPortUdh(int theDestPort, int theOrigPort)
+    {
+        byte[] udh = new byte[2];
+
+        udh[0] = (byte) (theDestPort & 0xff);
+        udh[1] = (byte) (theOrigPort & 0xff);
+
+        return new SmsUdhElement(SmsConstants.UDH_IEI_APP_PORT_8BIT, udh);
+    }
+
+    /**
+     * Creates a "16 bit Application Port Adressing" UDH element
+     * using UDH_IEI_APP_PORT_16BIT
+     * <p>
+     * Note! Only values between 0 and 16999 are usable, the rest of the port
+     * numbers are marked as reserved.
+     * <p>
+     * <b>Special handling in concatenated messages:</b><br>
+     * <i>
+     * In the case where this IE is to be used in a concatenated SM then the
+     * IEI, its associated IEI length and IEI data shall be contained in the
+     * first segment of the concatenated SM. The IEI, its associated IEI length
+     * and IEI data shall also be contained in every subsequent segment of the
+     * concatenated SM.
+     * </i>
+     * @param theDestPort Destination port
+     * @param theOrigPort Source port
+     * @return A SmsUdhElement
+     */
+    public static final SmsUdhElement get16BitApplicationPortUdh(int theDestPort, int theOrigPort)
+    {
+        byte[] udh = new byte[4];
+
+        udh[0] = (byte) (theDestPort & 0xff);
+        udh[1] = (byte) ((theDestPort >> 8) & 0xff);
+        udh[2] = (byte) (theOrigPort & 0xff);
+        udh[3] = (byte) ((theOrigPort >> 8) & 0xff);
+
+        return new SmsUdhElement(SmsConstants.UDH_IEI_APP_PORT_16BIT, udh);
     }
 }
