98c5c3504a1e644e84fe5e5b54e2d5de1030e74b
c95men <c95men@fa5118ba-f80e-0410-a4dc-9a36c028f66c>
2002-07-13 13:02:05 +0000

    Changed to MPL with permission from Boris

--- smsj/src/org/marre/sms/nokia/NokiaRingTone.java
+++ smsj/src/org/marre/sms/nokia/NokiaRingTone.java
@@ -1,22 +1,25 @@
-/*
-    SMS Library for the Java platform
-    Copyright (C) 2002  Markus Eriksson
-    Copyright (C) 2002  Boris von Loesch
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is "SMS Library for the Java platform".
+ *
+ * The Initial Developer of the Original Code is Boris von Loesch.
+ * Portions created by the Initial Developer are Copyright (C) 2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
 package org.marre.sms.nokia;
 
 import java.io.*;
