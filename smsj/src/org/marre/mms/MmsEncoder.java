bdd51f69f014517fe93b9524acdf64293ef9bdeb
wammy <wammy@fa5118ba-f80e-0410-a4dc-9a36c028f66c>
2003-07-25 14:07:06 +0000

    - Initial implementation of an MmsMessage encoder. This object encodes   a given MmsMessage object to the underlying WAP protocol.

---
+++ smsj/src/org/marre/mms/MmsEncoder.java
@@ -0,0 +1,106 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is "SMS Library for the Java platform".
+ *
+ * The Initial Developer of the Original Code is Markus Eriksson.
+ * Portions created by the Initial Developer are Copyright (C) 2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.marre.mms;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+
+import org.marre.mime.MimeHeader;
+import org.marre.mime.MimeMultipart;
+import org.marre.mime.encoder.wap.WapMimeEncoder;
+import org.marre.wap.MmsHeaderEncoder;
+
+/**
+ * @author Lincoln Spiteri 
+ */
+public class MmsEncoder 
+{
+
+	public MmsEncoder() 
+	{
+
+	}
+
+	public byte[] createMRetrievMessage(MmsMessage message){
+	
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		MimeMultipart messageBody = message.getMessage();
+		
+		try 
+		{
+			//Headers
+			MmsHeaderEncoder.writeHeaderXMmsMessageType(baos, "m-retrieve-conf");
+			MmsHeaderEncoder.writeHeaderXMmsMmsVersion(baos, "1.0");
+			
+			//LINCOLN: X-Mms-Transaction-ID is optional so it is left out for now
+			//LINCOLN: Message-ID is optional so it is left out for now
+			MmsHeaderEncoder.writeHeaderDate(baos, new Date());
+			MmsHeaderEncoder.writeHeaderFrom(baos, message.getFrom());
+			MmsHeaderEncoder.writeHeaderTo(baos, message.getTo());
+			MmsHeaderEncoder.writeHeaderSubject(baos, message.getSubject());
+			MmsHeaderEncoder.writeHeaderXMmsMessageClass(baos, message.getMessageClass());
+			MmsHeaderEncoder.writeHeaderXMmsPriority(baos, message.getPriority());
+			//MmsHeaderEncoder.writeHeaderXMmsDeliveryReport(baos, message.getDeliveryReport());
+			//MmsHeaderEncoder.writeHeaderXMmsReadReply(baos, message.getReadReply());
+			
+			if (message.isMultipartRelated())
+			{
+				MimeHeader theHeader = new MimeHeader("Content-type", "application/vnd.wap.multipart.related");
+			
+				//theHeader.addParam("type", "application/smil");
+				//theHeader.addParam("start", "<0000>");
+				MmsHeaderEncoder.writeHeaderContentType(baos, theHeader);
+			}
+			
+			if(messageBody != null)
+			{
+				WapMimeEncoder wme = new WapMimeEncoder();
+				wme.writeHeaders(baos, messageBody);
+				wme.writeData(baos, messageBody);
+				baos.close();
+			}			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return baos.toByteArray();
+	}
+
+	public static void main(String[] args) 
+	{
+	}
+}
