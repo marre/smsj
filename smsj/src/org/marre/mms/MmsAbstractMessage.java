b393637198f64ad7b1a46c7f4bf6eda5f1fbe60a
wammy <wammy@fa5118ba-f80e-0410-a4dc-9a36c028f66c>
2003-07-25 14:10:40 +0000

    *** empty log message ***

--- smsj/src/org/marre/mms/MmsAbstractMessage.java
+++ smsj/src/org/marre/mms/MmsAbstractMessage.java
@@ -43,7 +43,7 @@ import org.marre.mime.MimeContentType;
  * @author Markus Eriksson
  * @version $Id$
  */
-public abstract class MmsAbstractMessage implements MmsMessage 
+public abstract class MmsAbstractMessage
 {
 	private MimeContentType myContentType; 
 
