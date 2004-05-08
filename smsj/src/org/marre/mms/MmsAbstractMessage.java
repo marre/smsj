6322b1e60b63bee49733cb5d7c14d5a9dc735351
c95men <c95men@fa5118ba-f80e-0410-a4dc-9a36c028f66c>
2004-05-08 08:14:58 +0000

    extends MmsMessage

--- smsj/src/org/marre/mms/MmsAbstractMessage.java
+++ smsj/src/org/marre/mms/MmsAbstractMessage.java
@@ -43,7 +43,7 @@ import org.marre.mime.MimeContentType;
  * @author Markus Eriksson
  * @version $Id$
  */
-public abstract class MmsAbstractMessage
+public abstract class MmsAbstractMessage extends MmsMessage
 {
 	private MimeContentType myContentType; 
 
